490c76a3027a8c35b4a35f8229a3b568bf7d389b
Okky Permana <okkype@gmail.com>
2018-08-29 08:33:17 +0700

    add minimal ui
    Signed-off-by: Okky Permana <okkype@gmail.com>

--- wemos_sensor.ino
+++ wemos_sensor.ino
@@ -28,6 +28,13 @@ DHTesp dht;
 OneWire oneWire(D1);
 DallasTemperature sensors(&oneWire);
 
+void analogSwitch(byte a) {
+  digitalWrite(D5, a & 1);
+  digitalWrite(D6, a & 2);
+  digitalWrite(D7, a & 4);
+  digitalWrite(D8, a & 8);
+}
+
 void handleRoot() {
   delay(dht.getMinimumSamplingPeriod());
   ta = dht.getTemperature();
@@ -36,41 +43,69 @@ void handleRoot() {
   sensors.requestTemperatures();
   tw = sensors.getTempCByIndex(0);
   delay(1);
-  digitalWrite(D5, LOW);
-  digitalWrite(D6, LOW);
-  digitalWrite(D7, LOW);
-  digitalWrite(D8, LOW);
+  analogSwitch(0);
+  delay(1);
+  bl = ((double) analogRead(A0) / 1023.0) * 100.0;
+  delay(1);
+  analogSwitch(1);
   delay(1);
   ec = (double) analogRead(A0) * (25.0 / tw) * 0.002;
   delay(1);
-  digitalWrite(D5, HIGH);
-  digitalWrite(D6, LOW);
-  digitalWrite(D7, LOW);
-  digitalWrite(D8, LOW);
+  analogSwitch(2);
   delay(1);
   ph = (double) analogRead(A0) / 50.0;
-  delay(1);
-  digitalWrite(D5, LOW);
-  digitalWrite(D6, HIGH);
-  digitalWrite(D7, LOW);
-  digitalWrite(D8, LOW);
-  delay(1);
-  bl = ((double) analogRead(A0) / 1023.0) * 100.0;
 
   String out = "{\"id\":\"" + device_id + "\",\"type\":\"" + device_type + "\",\"value\":{";
   String out2 = "";
-  if (!isnan(ph) && (ph > 0)) out2 += " \"PH\":\"" + String(ph) + "\"";
+  if (!isnan(ph) && (ph >= 0.01)) out2 += " \"PH\":\"" + String(ph) + "\"";
   if (!isnan(ec) && (tw > -127) && (ec >= 0.01)) out2 += " \"EC\":\"" + String(ec) + "\"";
   if (!isnan(ta)) out2 += " \"TA\":\"" + String(ta) + "\"";
   if (!isnan(tw) && (tw > -127)) out2 += " \"TW\":\"" + String(tw) + "\"";
   if (!isnan(ht)) out2 += " \"HT\":\"" + String(ht) + "\"";
-  if (!isnan(bl) && (bl > 0)) out2 += " \"BL\":\"" + String(bl) + "\"";
+  if (!isnan(bl) && (bl >= 0.01)) out2 += " \"BL\":\"" + String(bl) + "\"";
   out2.trim();
   out2.replace(" ", ",");
 
   out += out2;
   out += "}}";
   server.send(200, "application/json", out);
+}
+
+void handleUI() {
+  String out = "";
+  out += "<html>";
+  out += "  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">";
+  out += "  <body>";
+  out += "    <pre>";
+  out += "Keasaman      (PH) : <span id=\"PH\"></span><br/>";
+  out += "Konduktivitas (EC) : <span id=\"EC\"></span><br/>";
+  out += "Suhu Air      (TW) : <span id=\"TW\"></span><br/>";
+  out += "Suhu Udara    (TA) : <span id=\"TA\"></span><br/>";
+  out += "Kelembapan    (HT) : <span id=\"HT\"></span><br/>";
+  out += "Baterai       (BL) : <span id=\"BL\"></span><br/>";
+  out += "    </pre>";
+  out += "    <script>";
+  out += "      function reloadSensor() {";
+  out += "        var xhttp = new XMLHttpRequest();";
+  out += "        xhttp.onreadystatechange = function() {";
+  out += "          if (this.readyState == 4 && this.status == 200) {";
+  out += "            var datas = JSON.parse(this.responseText);";
+  out += "            for (var key in datas.value) {";
+  out += "              if (datas.value.hasOwnProperty(key)) {";
+  out += "                document.getElementById(key).innerHTML = datas.value[key];";
+  out += "              }";
+  out += "            }";
+  out += "            setTimeout(reloadSensor, 1000);";
+  out += "          }";
+  out += "        };";
+  out += "        xhttp.open(\"GET\", \"/\", true);";
+  out += "        xhttp.send();";
+  out += "      }";
+  out += "      reloadSensor();";
+  out += "    </script>";
+  out += "  </body>";
+  out += "</html>";
+  server.send(200, "text/html", out);
 }
 
 void handleNotFound() {
@@ -132,6 +167,7 @@ void setup() {
   dht.setup(D2, DHTesp::DHT11);
 
   server.on("/", handleRoot);
+  server.on("/ui", handleUI);
   server.onNotFound(handleNotFound);
   server.begin();
   
